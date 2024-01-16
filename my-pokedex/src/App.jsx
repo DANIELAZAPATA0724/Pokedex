87346972d477ce92523e121ba77a169ff2d425c2
DANIELAZAPATA0724 <dzapata0724@gmail.com>
2024-01-16 11:49:35 +0100

    pokedex

---
+++ my-pokedex/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Navbar from './components/Navbar'
+import Pokemones from './components/Pokemones'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Pokemones />
+    </>
+  )
+}
+
+export default App
